06a5d7d617b7743f313df73db40c7abab38b8ba6
yeezus12 <163136465+yeezus12@users.noreply.github.com>
2024-08-28 07:58:37 -0300

    yee

---
+++ index.js
@@ -0,0 +1,29 @@
+var nome="eric"
+
+let idade=25;
+
+const pi=3.14;
+//
+let a=5; 
+let b=10;
+let soma= a+b
+console.log(soma)
+
+let a=5;
+let b=10;
+function multiplica(x,y) {
+    return x*y;
+}
+
+let produto= multiplca(a,b);
+console.log(produto);
+
+let frutas=['apple', 'banana', 'cherry'];
+console.log(frutas[1])
+
+let pessoa={
+    primeironome: 'eric'
+    sobrenome: 'Santos'
+    age:18 
+};
+console.log(pessoa.primeiroNome);
